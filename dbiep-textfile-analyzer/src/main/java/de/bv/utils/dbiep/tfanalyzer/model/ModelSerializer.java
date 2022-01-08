a0741d2f8c5a7acc04c89016e52a85c7d7465d83
Bernhard Volz <bernhard.volz@gmx.de>
2022-01-08 09:25:18 +0100

    #dbiep-4 TFAnalyzer: add complete configuration information
    - fix for showing tests in IntelliJ as being terminated
    - updated JUnit dependency
    - configured TRACE level for logging within tests

--- dbiep-textfile-analyzer/src/main/java/de/bv/utils/dbiep/tfanalyzer/model/ModelSerializer.java
+++ dbiep-textfile-analyzer/src/main/java/de/bv/utils/dbiep/tfanalyzer/model/ModelSerializer.java
@@ -5,8 +5,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import lombok.Getter;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.List;
 
 public class ModelSerializer {
@@ -28,10 +30,22 @@ public class ModelSerializer {
 
     public void serialize(final Configuration configuration) throws IOException {
         try {
+            //
+            // NOTE 1: writeValue() will close the stream object passed to it.
+            //         Since we want to output the configuration to the console,
+            //         we need to create a temporary output stream used for buffering
+            //         the output which will then be written to the console.
+            // NOTE 2: While closing the output stream won't be an issue for the
+            //         test being run using Maven, it is for IntelliJ.
+            //
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
             getObjectMapper().writeValue(
-                    System.out,
+                    outputStream,
                     configuration
             );
+
+            System.out.println(outputStream);
         } catch (IOException e) {
             e.printStackTrace();
             throw e;
